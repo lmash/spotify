525e0a1379ff11c128bc5d8a0ddd1de8c97e4d1f
mashie <lmachet@yahoo.com>
2023-12-03 18:17:25 +0000

    Fix Spotify authorisation. Create separate functions depending on whether will be searching or amending a users details

--- credentials.py
+++ credentials.py
@@ -1,18 +1,36 @@
 import logging
-import os
 
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-scope = 'user-library-modify'
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 logger = logging.getLogger(__name__)
+# Load the Spotify env variables from .env
 load_dotenv()
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
 
-auth_manager = SpotifyClientCredentials(
-    client_id=client_id, client_secret=client_secret
-)
-spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+def spotify_get() -> spotipy.Spotify:
+    """
+    Spotify's Client Credentials Flow - used when we search.
+    https://spotipy.readthedocs.io/en/2.22.1/#authorization-code-flow
+    Only endpoints that do not access user information can be accessed.
+    The advantage here in comparison with requests to the Web API made without an access token,
+    is that a higher rate limit is applied
+    """
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    return sp
+
+
+def spotify_put() -> spotipy.Spotify:
+    """
+    Spotify's Client Credentials Flow - used when we search.
+    https://spotipy.readthedocs.io/en/2.22.1/#authorization-code-flow
+    Only endpoints that do not access user information can be accessed.
+    The advantage here in comparison with requests to the Web API made without an access token,
+    is that a higher rate limit is applied
+    """
+    scope = "user-library-read"
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    return sp
